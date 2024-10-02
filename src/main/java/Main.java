ec51404e244db959ba3ea301f03004d2ca3c31b4
Aditi311555 <aditi.v.parekar@gmail.com>
2024-10-02 08:55:35 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,21 @@ public class Main {
   private static final Gson gson = new Gson();
 
   public static void main(String[] args) throws Exception {
+
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
     String command = args[0];
     if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
-      //  String bencodedValue = args[1];
-      //  String decoded;
-      //  try {
-      //    decoded = decodeBencode(bencodedValue);
-      //  } catch(RuntimeException e) {
-      //    System.out.println(e.getMessage());
-      //    return;
-      //  }
-      //  System.out.println(gson.toJson(decoded));
+      // Uncomment this block to pass the first stage
+        String bencodedValue = args[1];
+        String decoded;
+        try {
+          decoded = decodeBencode(bencodedValue);
+        } catch(RuntimeException e) {
+         System.out.println(e.getMessage());
+         return;
+        }
+        System.out.println(gson.toJson(decoded));
 
     } else {
       System.out.println("Unknown command: " + command);
@@ -27,6 +28,7 @@ public class Main {
   }
 
   static String decodeBencode(String bencodedString) {
+
     if (Character.isDigit(bencodedString.charAt(0))) {
       int firstColonIndex = 0;
       for(int i = 0; i < bencodedString.length(); i++) { 
