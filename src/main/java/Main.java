a9cb43db6f9a79373af5114096c13adc00d30484
Aditi Vinod Parekar <aditi.v.parekar@gmail.com>
2024-10-28 06:39:59 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     private static final Gson gson = new Gson();
+    private static int currentIndex = 0; // To keep track of the position in the bencoded string
 
-    public static void main(String[] args) throws Exception {
-        // You can use print statements for debugging; they will be visible when running tests.
-        // System.out.println("Logs from your program will appear here!"); // Comment out or remove this line
-        
+    public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Usage: decode <bencodedString>");
             return;
@@ -17,35 +17,52 @@ public class Main {
             String bencodedValue = args[1];
             Object decoded;
             try {
+                currentIndex = 0; // Reset index for each decode call
                 decoded = decodeBencode(bencodedValue);
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
                 return;
             }
             // Output should be formatted as expected
-            System.out.println(gson.toJson(decoded)); // Make sure the output is strictly the decoded string
+            System.out.println(gson.toJson(decoded));
         } else {
             System.out.println("Unknown command: " + command);
         }
     }
 
     static Object decodeBencode(String bencodedString) {
-        if (Character.isDigit(bencodedString.charAt(0))) {
-            int firstColonIndex = 0;
-            for (int i = 0; i < bencodedString.length(); i++) {
-                if (bencodedString.charAt(i) == ':') {
-                    firstColonIndex = i;
-                    break;
-                }
-            }
-            int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
-        }else if(bencodedString.startsWith("i"))
-        	{return Long.parseLong(bencodedString.substring(1,bencodedString.indexOf("e")));
-        	}
-        else {
-        	}
-            throw new RuntimeException("Only strings are supported at the moment");
-        }
-    }
+        char startChar = bencodedString.charAt(currentIndex);
 
+        // Decode a string
+        if (Character.isDigit(startChar)) {
+            int firstColonIndex = currentIndex;
+            while (bencodedString.charAt(firstColonIndex) != ':') {
+                firstColonIndex++;
+            }
+            int length = Integer.parseInt(bencodedString.substring(currentIndex, firstColonIndex));
+            currentIndex = firstColonIndex + 1;
+            String str = bencodedString.substring(currentIndex, currentIndex + length);
+            currentIndex += length;
+            return str;
+        }
+        // Decode an integer
+        else if (startChar == 'i') {
+            int endIndex = bencodedString.indexOf('e', currentIndex);
+            long number = Long.parseLong(bencodedString.substring(currentIndex + 1, endIndex));
+            currentIndex = endIndex + 1;
+            return number;
+        }
+        // Decode a list
+        else if (startChar == 'l') {
+            currentIndex++;
+            List<Object> list = new ArrayList<>();
+            while (bencodedString.charAt(currentIndex) != 'e') {
+                list.add(decodeBencode(bencodedString)); // Decode each element in the list
+            }
+            currentIndex++; // Move past 'e'
+            return list; // Return the decoded list
+        }
+        
+        throw new RuntimeException("Invalid bencoded format.");
+    }
+}
